4eac8efa91da5d8d2a50666d88b56191040d7a78
zcoco <zheng_keke@126.com>
2018-09-28 13:20:00 +0800

    调整项目内容
    调整项目内容

---
+++ web-module/gpmall-auction/src/main/java/com/gupao/edu/auction/controller/AuctionController.java
@@ -0,0 +1,14 @@
+package com.gupao.edu.auction.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/*
+ * 描述: 发起竞价申请
+ * @auth zhengkk19738
+ * @time 2018/9/28
+ */
+@RestController
+public class AuctionController {
+
+
+}
