e607bdccfdf3716a5ec9cdbcb64af85834ed2b51
zcoco <zheng_keke@126.com>
2018-10-11 17:13:02 +0800

    添加事物管理

--- service-module/auction-service-module/auction-service-provider/src/test/java/com/gupao/edu/auction/savetest/AuctionDetailServiceTest.java
+++ service-module/auction-service-module/auction-service-provider/src/test/java/com/gupao/edu/auction/savetest/AuctionDetailServiceTest.java
@@ -1,15 +1,16 @@
 package com.gupao.edu.auction.savetest;
 
 import com.gupao.edu.auction.AuctionModelEnum;
-import com.gupao.edu.auction.dal.entity.AuctionDetail;
 import com.gupao.edu.auction.dto.AuctionDetailRequest;
 import com.gupao.edu.auction.dto.AuctionDetailResponse;
 import com.gupao.edu.auction.service.AuctionDetailService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
 import java.util.Date;
@@ -20,8 +21,9 @@ import java.util.Date;
  * @time 2018/10/11
  */
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = "classpath:META-INF/spring/service-common.xml")
-/*@Transactional*/
+@ContextConfiguration(locations = {"classpath:META-INF/spring/service-common.xml", "classpath:META-INF/spring/service-transaction.xml"})
+@Transactional(transactionManager = "transactionManager")
+@Rollback(true)
 public class AuctionDetailServiceTest {
 
 
